2fd6771d526395831bdc81d86f8929ae2f05249d
fionamei131@gmail.com <fionamei131@gmail.com>
2022-12-03 16:02:09 -0500

    added overwritten changes

--- Prototype/pokemon/src/App.js
+++ Prototype/pokemon/src/App.js
@@ -51,10 +51,19 @@ return(
         <h1>LOGIN WITH GOOGLE</h1>
         <Login />
         <PokeInfo />
+        <input value={zip} onChange={handleChange} />
+        {zip && <button onClick={fetchData}>Click</button>}
+            {'pokemonData' in data && <div>
+              <h2>Pokemon: {data.pokemonData.name}</h2>
+              <h2>Image: <img src={data.pokemonData.image}/> </h2>
+              <h2>Id: {data.pokemonData.id}</h2>
+              <h2>Type: {data.pokemonData.type}</h2>
+              <h2>Weather: {data.weather}</h2>
+              <h2>Temperature: {data.temperature}</h2>
+              </div>
+              }
       </div>
-      }
-
-  </div>)
+  )
 
   
     
